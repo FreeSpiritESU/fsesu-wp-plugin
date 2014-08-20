7a05c54f1245d68cd4f13e3e3f9211ad9dc06dcc
Richard Perry <richard@perrymail.me.uk>
2014-08-20 09:04:43 +0000

    Setup basic plugin information and git files

---
+++ fsesu.php
@@ -0,0 +1,33 @@
+<?php
+/* 
+ Plugin Name:       FreeSpirit ESU Plugin
+ Description:       Custom Plugin designed for use by FreeSpirit ESU only
+ Version:           0.1.0
+ Author:            Richard Perry
+ Author URI:        http://richard.perry-online.me.uk/
+ License:           GPL2+
+ License URI:       http://www.gnu.org/licenses/gpl-2.0.html
+ Text Domain:       fsesu
+ GitHub Theme URI:  richardp2/fsesu-theme
+ GitHub Branch:     develop
+
+
+    Copyright 2014  FreeSpirit ESU  (email : richard@freespiritesu.org.uk)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+ 
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
